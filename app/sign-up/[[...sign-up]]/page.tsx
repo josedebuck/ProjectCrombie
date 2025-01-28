4dc2f2a51dcd284845338f938d7817b6a1e3938d
josedebuck <debuckjose@gmail.com>
2025-01-28 20:01:56 -0300

    cambiando configuracion de clerk

--- app/sign-up/[[...sign-up]]/page.tsx
+++ app/sign-up/[[...sign-up]]/page.tsx
@@ -1,6 +1,18 @@
-import { SignUp } from "@clerk/nextjs";
+import { SignUp, useClerk } from "@clerk/nextjs";
+import { useEffect } from "react";
+import { useRouter } from "next/router";
 
 export default function Page() {
+  const { user } = useClerk();
+  const router = useRouter();
+
+  useEffect(() => {
+    if (user) {
+      // Si el usuario está autenticado, redirigir a la página de inicio
+      router.push("/homepage");
+    }
+  }, [user, router]);
+
   return (
     <div className="h-[calc(100vh-96px)] flex items-center justify-center">
       <SignUp />
