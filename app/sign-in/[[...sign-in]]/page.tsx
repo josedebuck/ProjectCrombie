9c6c30eab909d6cdac0b350165e22b4c17a3edec
josedebuck <debuckjose@gmail.com>
2025-01-28 20:21:26 -0300

    change signin and signup

--- app/sign-in/[[...sign-in]]/page.tsx
+++ app/sign-in/[[...sign-in]]/page.tsx
@@ -1,20 +1,6 @@
-"use client";  // Esto marca el componente como cliente
-
-import { SignIn, useClerk } from "@clerk/nextjs";
-import { useEffect } from "react";
-import { useRouter } from "next/router";
+import { SignIn } from "@clerk/nextjs";
 
 export default function Page() {
-  const { user } = useClerk();
-  const router = useRouter();
-
-  useEffect(() => {
-    if (user) {
-      // Si el usuario está autenticado, redirigir a la página de inicio
-      router.push("/homepage");
-    }
-  }, [user, router]);
-
   return (
     <div className="h-[calc(100vh-96px)] flex items-center justify-center">
       <SignIn />
